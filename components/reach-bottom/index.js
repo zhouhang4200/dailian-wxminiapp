cbf44bb8c6fa7f0733f33c1ef2e3e1ae67cfab27
xiaominggong <550843110@qq.com>
2018-08-25 15:12:30 +0800

    优化页面

---
+++ components/reach-bottom/index.js
@@ -0,0 +1,27 @@
+// components/reach-bottom/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    reachEndInfo: {
+      type: Object,
+      value: {
+        isHidden: true,
+        isMore: true,
+        isFail: false,
+        isLoading: false
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {}
+})
