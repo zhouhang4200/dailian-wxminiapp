8d2167cf74eada248143256f03e8bd14d603344b
xiaominggong <550843110@qq.com>
2018-08-29 09:32:29 +0800

    修改密码相关逻辑

--- pages/account/password/index.js
+++ pages/account/password/index.js
@@ -6,7 +6,38 @@ import {
   api_findPayPassword,
   api_updatePayPassword,
   api_settingPayPassword,
+  api_sendFindLoginPasswordPhoneCode,
+  api_sendFindPayPasswordPhoneCode
 } from '../../../lib/api'
+
+
+const setting = {
+  find_login: {
+    title: '找回登录密码',
+    type: 'find_login',
+    action: 'api_findLoginPassword'
+  },
+  update_login: {
+    title: '修改登录密码',
+    type: 'update_login',
+    action: 'api_updateLoginPassword'
+  },
+  find_pay: {
+    title: '找回支付密码',
+    type: 'find_pay',
+    action: 'api_findPayPassword'
+  },
+  update_pay: {
+    title: '修改支付密码',
+    type: 'update_pay',
+    action: 'api_updatePayPassword',
+  },
+  setting_pay: {
+    title: '设置支付密码',
+    type: 'setting_pay',
+    action: 'api_settingPayPassword'
+  }
+};
 
 Page({
 
@@ -17,54 +48,68 @@ Page({
     ...Utils.verificationCode.data,
 
     action: '',
-    type: ''
+    type: '',
+    phone: ''
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   *找回登录密码
    */
-  onLoad: function (options) {
-    const action = options.action;
-    const setting = {
-      find_login: {
-        title: '找回登录密码',
-        type: 'find_login',
-        action: 'api_findLoginPassword'
-      },
-      update_login: {
-        title: '修改登录密码',
-        type: 'update_login',
-        action: 'api_updateLoginPassword'
-      },
-      find_pay: {
-        title: '找回支付密码',
-        type: 'find_pay',
-        action: 'api_findPayPassword'
-      },
-      update_pay: {
-        title: '修改支付密码',
-        type: 'update_pay',
-        action: 'api_updatePayPassword',
-      },
-      setting_pay: {
-        title: '设置支付密码',
-        type: 'setting_pay',
-        action: 'api_settingPayPassword'
+  onFindLoginPassword: function (e) {
+    const formData = e.detail.value;
+    const {phone, new_password, confirm_password, verification_code} = formData;
+    const isValidateForm = function () {
+      const requiredValidate = {
+        phone: '请输入手机号码',
+        new_password: '请输入新登录密码',
+        confirm_password: '请再次输入新密码',
+      };
+      if (!(Utils.Regrex.phone.test(phone)) && phone) {
+        wx.showToast({title: '请输入正确的手机号码', icon: 'none'});
+        return false;
       }
+      if (new_password.length < 6 && new_password) {
+        wx.showToast({title: '密码长度最低为6位', icon: 'none'});
+        return false;
+      }
+      for (let key in formData) {
+        if (formData[key] === '') {
+          wx.showToast({title: requiredValidate[key], icon: 'none'});
+          return false;
+        }
+      }
+      if (new_password !== confirm_password) {
+        wx.showToast({title: '两次密码输入不一致，请检查', icon: 'none'});
+        return false;
+      }
+      return true
     };
-    this.setData({});
-    wx.setNavigationBarTitle({
-      title: '忘记密码'
-    });
-    const settingValue = setting[action];
-    if (setting[action]) {
-      this.setData({
-        action: settingValue.action,
-        type: settingValue.type,
+    if (isValidateForm()) {
+      wx.showLoading({title: '加载中', icon: 'none'});
+      api_findLoginPassword({
+        phone,
+        new_password,
+        verification_code
+      }).then(data => {
+        wx.hideLoading();
+        if (data.code) {
+          return wx.showToast({title: data.message, icon: 'none'})
+        }
+        wx.showModal({
+          showCancel: false,
+          content: '密码找回成功，请重新登录',
+          success: function (res) {
+            if (res.confirm) {
+              wx.removeStorageSync('token');
+              wx.redirectTo({
+                url: '/pages/account/login/index'
+              })
+            }
+          }
+        });
       })
     }
   },
-
 
   /**
    * 修改登录密码
@@ -72,8 +117,8 @@ Page({
    */
   onUpdateLoginPassword: function (e) {
     const formData = e.detail.value;
+    const {old_password, new_password, confirm_password} = formData;
     const isValidateForm = function () {
-      const {old_password, new_password, confirm_password} = formData;
       const requiredValidate = {
         old_password: '请输入旧密码',
         new_password: '请输入新密码',
@@ -97,14 +142,17 @@ Page({
     };
     if (isValidateForm()) {
       wx.showLoading({title: '加载中', icon: 'none'});
-      api_updateLoginPassword(formData).then(data => {
+      api_updateLoginPassword({
+        old_password,
+        new_password
+      }).then(data => {
         wx.hideLoading();
         if (data.code) {
           return wx.showToast({title: data.message, icon: 'none'})
         }
         wx.showModal({
           showCancel: false,
-          content: '修改成功，请重新登录',
+          content: '密码修改成功，请重新登录',
           success: function (res) {
             if (res.confirm) {
               wx.removeStorageSync('token');
@@ -119,19 +167,22 @@ Page({
   },
 
   /**
-   * 修改支付密码
-   * @param e
+   *找回支付密码
    */
-  onUpdatePayPassword: function (e) {
+  onFindPayPassword: function (e) {
     const formData = e.detail.value;
+    const {phone, new_password, confirm_password, verification_code} = formData;
     const isValidateForm = function () {
-      const {old_pay_password, new_pay_pasword, confirm_password} = formData;
       const requiredValidate = {
-        old_pay_password: '请输入旧支付密码',
-        new_pay_pasword: '请输入新支付密码',
+        phone: '请输入手机号码',
+        new_password: '请输入新登录密码',
         confirm_password: '请再次输入新密码',
       };
-      if (new_pay_pasword.length < 6 && new_pay_pasword) {
+      if (!(Utils.Regrex.phone.test(phone)) && phone) {
+        wx.showToast({title: '请输入正确的手机号码', icon: 'none'});
+        return false;
+      }
+      if (new_password.length < 6 && new_password) {
         wx.showToast({title: '密码长度最低为6位', icon: 'none'});
         return false;
       }
@@ -141,7 +192,7 @@ Page({
           return false;
         }
       }
-      if (new_pay_pasword !== confirm_password) {
+      if (new_password !== confirm_password) {
         wx.showToast({title: '两次密码输入不一致，请检查', icon: 'none'});
         return false;
       }
@@ -149,20 +200,163 @@ Page({
     };
     if (isValidateForm()) {
       wx.showLoading({title: '加载中', icon: 'none'});
-      api_updatePayPassword(formData).then(data => {
+      api_findPayPassword({
+        phone,
+        new_password,
+        verification_code
+      }).then(data => {
         wx.hideLoading();
         if (data.code) {
           return wx.showToast({title: data.message, icon: 'none'})
         }
         wx.showModal({
           showCancel: false,
-          content: '修改成功',
+          content: '密码找回成功',
           success: function (res) {
             if (res.confirm) {
-
+              wx.navigateBack();
             }
           }
         });
+      })
+    }
+  },
+
+  /**
+   * 修改支付密码
+   * @param e
+   */
+  onUpdatePayPassword: function (e) {
+    const formData = e.detail.value;
+    const {old_pay_password, new_pay_password, confirm_password} = formData;
+    const isValidateForm = function () {
+      const requiredValidate = {
+        old_pay_password: '请输入旧支付密码',
+        new_pay_password: '请输入新支付密码',
+        confirm_password: '请再次输入新密码',
+      };
+      if (new_pay_password.length < 6 && new_pay_password) {
+        wx.showToast({title: '密码长度最低为6位', icon: 'none'});
+        return false;
+      }
+      for (let key in formData) {
+        if (formData[key] === '') {
+          wx.showToast({title: requiredValidate[key], icon: 'none'});
+          return false;
+        }
+      }
+      if (new_pay_password !== confirm_password) {
+        wx.showToast({title: '两次密码输入不一致，请检查', icon: 'none'});
+        return false;
+      }
+      return true
+    };
+    if (isValidateForm()) {
+      wx.showLoading({title: '加载中', icon: 'none'});
+      api_updatePayPassword({
+        old_pay_password,
+        new_pay_password
+      }).then(data => {
+        wx.hideLoading();
+        if (data.code) {
+          return wx.showToast({title: data.message, icon: 'none'})
+        }
+        return wx.showToast({title: '修改成功', icon: 'none'})
+      })
+    }
+  },
+
+  /**
+   *设置支付密码
+   */
+  onSettingPayPassword: function (e) {
+    const formData = e.detail.value;
+    const {pay_password, confirm_password} = formData;
+    const isValidateForm = function () {
+      const requiredValidate = {
+        new_password: '请输入支付密码',
+        confirm_password: '请再次输入新密码',
+      };
+      if (pay_password.length < 6 && pay_password) {
+        wx.showToast({title: '密码长度最低为6位', icon: 'none'});
+        return false;
+      }
+      for (let key in formData) {
+        if (formData[key] === '') {
+          wx.showToast({title: requiredValidate[key], icon: 'none'});
+          return false;
+        }
+      }
+      if (pay_password !== confirm_password) {
+        wx.showToast({title: '两次密码输入不一致，请检查', icon: 'none'});
+        return false;
+      }
+      return true
+    };
+    if (isValidateForm()) {
+      wx.showLoading({title: '加载中', icon: 'none'});
+      api_settingPayPassword({
+        pay_password
+      }).then(data => {
+        wx.hideLoading();
+        if (data.code) {
+          return wx.showToast({title: data.message, icon: 'none'})
+        }
+        wx.showModal({
+          showCancel: false,
+          content: '支付密码设置成功',
+          success: function (res) {
+            if (res.confirm) {
+              wx.navigateBack();
+            }
+          }
+        });
+      })
+    }
+  },
+
+  onPhoneInput: function (e) {
+    this.setData({phone: e.detail.value})
+  },
+
+  /**
+   * 发送手机验证码
+   * @returns {boolean}
+   */
+  sendPhoneCode: function () {
+    const phone = this.data.phone;
+    if (this.data.verificationCode.disabled) {
+      return false;
+    }
+    if (!(/^[1][3,4,5,7,8][0-9]{9}$/.test(phone))) {
+      wx.showToast({title: '请输入正确的手机号码', icon: 'none'});
+      return false;
+    }
+    this.verificationCodeSend();
+    wx.showLoading();
+    let api = this.data.type === 'find_login' ? api_sendFindLoginPasswordPhoneCode : api_sendFindPayPasswordPhoneCode;
+    api({phone}).then(data => {
+      wx.hideLoading();
+      if (data.code) {
+        return this.verificationCodeFail(data.message);
+      }
+      return this.verificationCodeSuccess()
+    }, () => this.verificationCodeFail())
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const action = options.action;
+    wx.setNavigationBarTitle({
+      title: '忘记密码'
+    });
+    const settingValue = setting[action];
+    if (setting[action]) {
+      this.setData({
+        action: settingValue.action,
+        type: settingValue.type,
       })
     }
   },
